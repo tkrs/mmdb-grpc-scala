a9c4c5361d15159d2e54762c90176e6f082c2036
Takeru Sato <type.in.type@gmail.com>
2024-09-08 00:43:31 +0000

    build(deps): bump scala-library from 2.12.19 to 2.12.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val V = new {
-    val `scala2.12` = "2.12.19"
+    val `scala2.12` = "2.12.20"
     val `scala2.13` = "2.13.14"
     val scala3      = "3.3.3"
 
