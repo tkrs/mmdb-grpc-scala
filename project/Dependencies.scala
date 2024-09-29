579ffd1c03fe018a9cdbbde650192e5de693942e
Takeru Sato <type.in.type@gmail.com>
2024-09-29 00:46:20 +0000

    build(deps): bump scala3-library from 3.3.3 to 3.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val V = new {
     val `scala2.12` = "2.12.19"
     val `scala2.13` = "2.13.14"
-    val scala3      = "3.3.3"
+    val scala3      = "3.3.4"
 
     val gatling     = "3.9.5"
     val gatlingGrpc = "0.16.0"
